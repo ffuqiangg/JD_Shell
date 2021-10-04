f93a474bc162be9ebb573c1ecc55321a80c20b23
ffuqiangg <34062439+ffuqiangg@users.noreply.github.com>
2021-10-04 20:58:03 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,14 @@
 
 ## 路径
 dir_shell=/jd
+url_scripts=https://github.com/ffuqiangg/JD_Shell.git
 
 ## 导入函数变量
 . $dir_shell/share.sh
 
 link_shell
 set_config
+git clone $url_scripts $dir_scripts 
 update_crontab
 
 exec "$@"
