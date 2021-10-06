8944f6678ea2615cfe950f157aef3793aa00ab05
ffuqiangg <34062439+ffuqiangg@users.noreply.github.com>
2021-10-06 17:02:55 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -5,8 +5,8 @@ dir_shell=$(dirname $(readlink -f "$0"))
 url_scripts='https://github.com/shufflewzc/faker2.git'
 
 ## 导入变量函数，配置文件
-. $dir_shell/config/config.sh
 . $dir_shell/share.sh
+. $file_config
 
 ## 创建软链接
 link_shell
