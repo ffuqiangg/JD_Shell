5889be066d6778587767e7ede2e4aa1b6d256ba5
ffuqiangg <34062439+ffuqiangg@users.noreply.github.com>
2021-10-06 17:03:20 +0800

    Update task.sh

--- task.sh
+++ task.sh
@@ -4,8 +4,8 @@
 dir_shell=$(dirname $(readlink -f "$0"))
 
 ## 导入配置文件，通用函数变量
-. $dir_shell/config/config.sh
 . $dir_shell/share.sh
+. $file_config
 
 ## $1：文件名不带后缀 $2：=now取消随机延迟 
 if [[ $2 = now ]];then
