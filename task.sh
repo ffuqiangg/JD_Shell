6719ee87641f76fd6077890ec6dbd0ed4b684a03
ffuqiangg <34062439+ffuqiangg@users.noreply.github.com>
2021-10-07 20:56:41 +0800

    Update task.sh

--- task.sh
+++ task.sh
@@ -7,13 +7,75 @@ dir_shell=$(dirname $(readlink -f "$0"))
 . $dir_shell/share.sh
 . $file_config
 
-## $1：文件名不带后缀 $2：=now取消随机延迟 
-if [[ $2 = now ]];then
-	sleep_num=0
-else
-	sleep_num=$((RANDOM%300+1))
-fi
+## 使用说明
+usage () {
+    echo "task 命令使用说明："
+    echo "task <js_name>       # 运行脚本，如设置了延迟且不在0-2、29-31、59分内，将随机延迟一定秒数"
+    echo "task <js_name> now   # 立即运行脚本"
+}
 
-make_dir $dir_log/$1 && sleep $sleep_num; node $dir_scripts/$1.js 2>&1 | tee $dir_log/$1/$(date +%Y-%m-%d-%H-%M-%S).log
+## 随机延迟执行函数
+random_delay () {
+	local random_delay_max=$RandomDelay
+	if [[ $random_delay_max ]] && [[ $random_delay_max -gt 0 ]]; then
+		local current_min=$(date "+%-M")
+		if [[ $current_min -gt 2 && $current_min -lt 29 ]] || [[ $current_min -gt 31 && $current_min -lt 59 ]]; then
+			delay_second=$((RANDOM % $random_delay_max + 1))
+			echo -e "\n命令未添加 \"now\"，随机延迟 $delay_second 秒后再执行任务，如需立即终止，请按 CTRL+C...\n"
+			sleep $delay_second
+		fi
+	fi
+}
 
-exit 0
+## 正常运行脚本
+run_normal () {
+	local task_name=$1
+	make_dir $dir_log/$task_name
+	node $dir_scripts/$task_name.js 2>&1 | tee $$dir_log/$task_name/$(date +%Y-%m-%d-%H-%M-%S).log
+}
+
+## 多合一签到脚本函数
+run_bean_sign () {
+	local dir_current=$(pwd)
+	local task_name=$1
+	make_dir $dir_log/$task_name
+	cd $dir_scripts
+	node $task_name.js 2>&1 | tee $$dir_log/$task_name/$(date +%Y-%m-%d-%H-%M-%S).log
+	cd $dir_current
+}
+
+main () {
+	case $# in
+		0)
+			usage
+			;;
+		1)
+			case $1 in
+				jd_bean_sign)
+					run_bean_sign $1
+					;;
+				*)
+					random_delay
+					run_normal $1
+					;;
+			esac
+			;;
+		2)
+			case $2 in
+				now)
+					run_normal $1
+					;;
+				*)
+					echo -e "\n命令输入错误...\n"
+					usage
+					;;
+			esac
+			;;
+		*)
+			echo -e "\n命令过多...\n"
+			usage
+			;;
+	esac
+}
+
+main "$@"
