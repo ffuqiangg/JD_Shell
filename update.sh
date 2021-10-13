8736312c0a0e9ef1b86dbffaee5f301233f5930c
ffuqiangg <34062439+ffuqiangg@users.noreply.github.com>
2021-10-13 17:03:55 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -133,7 +133,7 @@ update_own_raw () {
 
 ## 新增定时任务
 add_cron () {
-	for add_cron_list in $(diff $scripts_list_old $scripts_list_new | grep "+" | grep -v '@\|scripts.list' | sed 's/+//g'); do
+	for add_cron_list in $(diff $scripts_list_old $scripts_list_new | grep ">" | sed 's/> //g'); do
 		if [[ -n $add_cron_list ]]; then
 			add_task_name=$(echo $add_cron_list | awk -F "/" '{print $NF}')
 			add_task_name=${add_task_name%%.*}
@@ -185,7 +185,7 @@ add_cron () {
 
 ## 删除失效任务
 del_cron () {
-	for del_cron_list in $(diff $scripts_list_old $scripts_list_new | grep "-" | grep -v '@\|scripts.list' | sed 's/-//g'); do
+	for del_cron_list in $(diff $scripts_list_old $scripts_list_new | grep "<" | sed 's/< //g'); do
 		if [[ -n $del_cron_list ]]; then
 			del_task_name=$(echo $del_cron_list | awk -F "/" '{print $NF}')
 			del_task_name=${del_task_name%%.*}
