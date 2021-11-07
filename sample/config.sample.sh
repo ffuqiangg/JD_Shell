3cb4ed333061c974d1897206c351b29871666588
ffuqiangg <34062439+ffuqiangg@users.noreply.github.com>
2021-11-07 15:15:38 +0800

    Update config.sample.sh

--- sample/config.sample.sh
+++ sample/config.sample.sh
@@ -1,25 +1,26 @@
 ## jd cookie
 export JD_COOKIE=''
 
-## TG_bot 通知参数
+## TG_bot 通知参数，其他渠道通知变量参数参照仓库 ENV.md 文件
 export TG_BOT_TOKEN=''
 export TG_USER_ID=''
 
 ## 保留脚本运行日志天数
 RmLogDaysAgo='7'
 
-## 随机延迟最大最小值，秒
-RandomMax='300'   # 最大值
-RandomMin='5'     # 最小值
+## 随机延迟最大最小值，单位：秒，最小值默认1
+## 最大值留空或为0则任务准时运行无延迟
+RandomMax='300'
+RandomMin=''
 
 ## 独立js脚本文件
 OwnRawFile=(
-  #https://raw.githubusercontent.com/Ariszy/Private-Script/master/JD/zy_xyzzh.js
+	#https://raw.githubusercontent.com/Ariszy/Private-Script/master/JD/zy_xyzzh.js
 )
 
 # ============= 长期活动变量 =============
-## 宠汪汪积分有就换
-export JOY_GET20WHEN16='true'
+## 宠汪汪积分兑换
+export JD_JOY_REWARD_NAME='500'
 ## 东东超市蓝币兑换
 export MARKET_COIN_TO_BEANS='超值'
 ## 摇钱树卖出金币
