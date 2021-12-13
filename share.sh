428bbd937fce2e9be2e2118b85edcfa59523322a
ffuqiangg <ffuqiangg@gmail.com>
2021-12-13 20:51:02 +0800

    fomart code

--- share.sh
+++ share.sh
@@ -16,63 +16,63 @@ scripts_list_new=$dir_list_tmp/scripts.list.new
 
 ## 软链接对应文件
 original_name=(
-  update.sh
-  task.sh
-  rmlog.sh
+    update.sh
+    task.sh
+    rmlog.sh
 )
 
 ## 创建目录，$1：目录绝对路径
 make_dir () {
-  local dir=$1
-  [ ! -d $dir ] && mkdir -p $dir
+    local dir=$1
+    [ ! -d $dir ] && mkdir -p $dir
 }
 
 ## 创建/修复 软链接
 link_shell () {
-  for ((i=0; i<${#original_name[*]}; i++)); do
-    link_name=/usr/local/bin/${original_name[i]%%.*}
-    shell_name=$dir_shell/${original_name[i]}
-    if [ ! -L $link_name ] || [[ $(readlink -f $link_name) != $shell_name ]]; then
-      rm -f $link_name 2>/dev/null
-      ln -sf $shell_name $link_name
-    fi
-  done
+    for ((i=0; i<${#original_name[*]}; i++)); do
+        link_name=/usr/local/bin/${original_name[i]%%.*}
+        shell_name=$dir_shell/${original_name[i]}
+        if [ ! -L $link_name ] || [[ $(readlink -f $link_name) != $shell_name ]]; then
+            rm -f $link_name 2>/dev/null
+            ln -sf $shell_name $link_name
+        fi
+    done
 }
 
 ## 设置/修复 权限
 shell_chmod () {
-  for ((i=0; i<${#original_name[*]}; i++)); do
-    if [[ ! -x $dir_shell/${original_name[i]} ]]; then
-      chmod +x $dir_shell/${original_name[i]}
-    fi
-  done
+    for ((i=0; i<${#original_name[*]}; i++)); do
+        if [[ ! -x $dir_shell/${original_name[i]} ]]; then
+            chmod +x $dir_shell/${original_name[i]}
+        fi
+    done
 }
 
 ## 发送通知 $1：标题，$2：正文
 send_notify () {
-  title=$(echo $1 | sed 's/-/_/g')
-  msg=$(echo -e $2)
-  node $dir_shell/notify.js "$title" "$msg"
+    title=$(echo $1 | sed 's/-/_/g')
+    msg=$(echo -e $2)
+    node $dir_shell/notify.js "$title" "$msg"
 }
 
 ## 更新crontab
 update_crontab () {
-  if [[ $(cat $file_crontab_user) != $(crontab -l) ]]; then
-    crontab $file_crontab_user
-  fi
+    if [[ $(cat $file_crontab_user) != $(crontab -l) ]]; then
+        crontab $file_crontab_user
+    fi
 }
 
 ## 生成随机数 $1：最大值
 make_random () {
-  local random_max=$1
-  random_num=$((RANDOM % $random_max + 1))
-  echo $random_num
+    local random_max=$1
+    random_num=$((RANDOM % $random_max + 1))
+    echo $random_num
 }
 
 ## 生成文件列表 $1：所在目录 $2：文件后缀 $3：存放列表文件
 create_list () {
-  local dir=$1
-  local filetype=$2
-  local filelist=$3
-  ls $dir/*.$filetype > $filelist
+    local dir=$1
+    local filetype=$2
+    local filelist=$3
+    ls $dir/*.$filetype > $filelist
 }
