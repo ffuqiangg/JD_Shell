8a190f1f8400deb93087b92c529c25013ee1d203
ffuqiangg <ffuqiangg@gmail.com>
2021-12-13 20:51:16 +0800

    fomart code

--- rmlog.sh
+++ rmlog.sh
@@ -9,31 +9,31 @@ dir_shell=$(dirname $(readlink -f "$0"))
 
 ## 删除运行js脚本的旧日志
 remove_js_log () {
-  local log_full_path_list=$(ls -l $dir_log/*/*.log | grep -v "bot" | awk '{print $9}')
-  local diff_time
-  for log in $log_full_path_list; do
-    local log_date=$(echo $log | awk -F "/" '{print $NF}' | cut -c 1-10)   #文件名比文件属性获得的日期要可靠
-    diff_time=$(($(date +%s) - $(date +%s -d "$log_date")))
-    [[ $diff_time -gt $((${RmLogDaysAgo} * 86400)) ]] && rm -vf $log
-  done
+    local log_full_path_list=$(ls -l $dir_log/*/*.log | grep -v "bot" | awk '{print $9}')
+    local diff_time
+    for log in $log_full_path_list; do
+        local log_date=$(echo $log | awk -F "/" '{print $NF}' | cut -c 1-10)   #文件名比文件属性获得的日期要可靠
+        diff_time=$(($(date +%s) - $(date +%s -d "$log_date")))
+        [[ $diff_time -gt $((${RmLogDaysAgo} * 86400)) ]] && rm -vf $log
+    done
 }
 
 ## 删除空文件夹
 remove_empty_dir () {
-  cd $dir_log
-  for dir in $(ls); do
-    if [ -d $dir ] && [[ -z $(ls $dir) ]]; then
-      rm -rf $dir
-    fi
-  done
+    cd $dir_log
+    for dir in $(ls); do
+        if [ -d $dir ] && [[ -z $(ls $dir) ]]; then
+            rm -rf $dir
+        fi
+    done
 }
 
 ## 运行
 if [[ ${RmLogDaysAgo} ]]; then
-  echo -e "查找旧日志文件中...\n"
-  remove_js_log
-  remove_empty_dir
-  echo -e "删除旧日志执行完毕\n"
+    echo -e "查找旧日志文件中...\n"
+    remove_js_log
+    remove_empty_dir
+    echo -e "删除旧日志执行完毕\n"
 fi
 
 exit 0
