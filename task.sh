d517e297fae041ed510f8631b3aa336b002481f7
ffuqiangg <ffuqiangg@gmail.com>
2021-12-13 20:50:49 +0800

    fomart code

--- task.sh
+++ task.sh
@@ -9,77 +9,77 @@ dir_shell=$(dirname $(readlink -f "$0"))
 
 ## 使用说明
 usage () {
-  echo "task 命令使用说明："
-  echo "task <js_name>       # 运行脚本，如设置了延迟且不在0-2、29-31、58分内，将随机延迟一定秒数"
-  echo "task <js_name> now   # 立即运行脚本"
+    echo "task 命令使用说明："
+    echo "task <js_name>       # 运行脚本，如设置了延迟且不在0-2、29-31、58分内，将随机延迟一定秒数"
+    echo "task <js_name> now   # 立即运行脚本"
 }
 
 ## 随机延迟执行函数
 random_delay () {
-  local random_delay_max=$RandomMax
-  if [[ $random_delay_max ]] && [[ $random_delay_max -gt 0 ]]; then
-    local current_min=$(date "+%-M")
-    if [[ $current_min -gt 2 && $current_min -lt 29 ]] || [[ $current_min -gt 31 && $current_min -lt 58 ]]; then
-      delay_second=$(make_random $random_delay_max)
-      echo -e "\n命令未添加 \"now\"，随机延迟 $delay_second 秒后再执行任务，如需立即终止，请按 CTRL+C...\n"
-      sleep $delay_second
+    local random_delay_max=$RandomMax
+    if [[ $random_delay_max ]] && [[ $random_delay_max -gt 0 ]]; then
+        local current_min=$(date "+%-M")
+        if [[ $current_min -gt 2 && $current_min -lt 29 ]] || [[ $current_min -gt 31 && $current_min -lt 58 ]]; then
+            delay_second=$(make_random $random_delay_max)
+            echo -e "\n命令未添加 \"now\"，随机延迟 $delay_second 秒后再执行任务，如需立即终止，请按 CTRL+C...\n"
+            sleep $delay_second
+        fi
     fi
-  fi
 }
 
 ## 正常运行脚本
 run_normal () {
-  local task_name=$1
-  if [[ -f $dir_scripts/$task_name.js ]]; then
-    make_dir $dir_log/$task_name
-    node $dir_scripts/$task_name.js 2>&1 | tee $dir_log/$task_name/$(date +%Y-%m-%d-%H-%M-%S).log
-  else
-    echo "脚本文件 $dir_scripts/$task_name.js 不存在"
-  fi
+    local task_name=$1
+    if [[ -f $dir_scripts/$task_name.js ]]; then
+        make_dir $dir_log/$task_name
+        node $dir_scripts/$task_name.js 2>&1 | tee $dir_log/$task_name/$(date +%Y-%m-%d-%H-%M-%S).log
+    else
+        echo "脚本文件 $dir_scripts/$task_name.js 不存在"
+    fi
 }
 
 ## 多合一签到脚本函数
 run_bean_sign () {
-  local dir_current=$(pwd)
-  local task_name=$1
-  make_dir $dir_log/$task_name
-  cd $dir_scripts
-  node $task_name.js 2>&1 | tee $dir_log/$task_name/$(date +%Y-%m-%d-%H-%M-%S).log
-  cd $dir_current
+    local dir_current=$(pwd)
+    local task_name=$1
+    make_dir $dir_log/$task_name
+    cd $dir_scripts
+    node $task_name.js 2>&1 | tee $dir_log/$task_name/$(date +%Y-%m-%d-%H-%M-%S).log
+    cd $dir_current
 }
 
 main () {
-  case $# in
-    0)
-      usage
-      ;;
-    1)
-      case $1 in
-        jd_bean_sign)
-          run_bean_sign $1
-          ;;
+    case $# in
+        0)
+            usage
+            ;;
+        1)
+            case $1 in
+                jd_bean_sign)
+                    run_bean_sign $1
+                    ;;
+                *)
+                    random_delay
+                    run_normal $1
+                    ;;
+            esac
+            ;;
+        2)
+            case $2 in
+                now)
+                    run_normal $1
+                    ;;
+                *)
+                    echo -e "\n命令输入错误...\n"
+                    usage
+                    ;;
+            esac
+            ;;
         *)
-          random_delay
-          run_normal $1
-          ;;
-      esac
-      ;;
-    2)
-      case $2 in
-        now)
-          run_normal $1
-          ;;
-        *)
-          echo -e "\n命令输入错误...\n"
-          usage
-          ;;
-      esac
-      ;;
-    *)
-      echo -e "\n命令过多...\n"
-      usage
-      ;;
-  esac
+            echo -e "\n命令过多...\n"
+            usage
+            ;;
+    esac
 }
 
 main "$@"
