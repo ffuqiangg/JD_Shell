596518224ad3f3f476d427356e49f9ffd89c36b3
ffuqiangg <ffuqiangg@gmail.com>
2021-12-25 20:02:24 +0800

    优化 add_cron 函数提取 cron 逻辑

--- update.sh
+++ update.sh
@@ -140,7 +140,11 @@ add_cron () {
                 add_task_word=$(sed -n '/^const \$/p' $add_cron_list | awk -F "'" '{print $2}')
             fi
             script_note_line=$(cat $add_cron_list | grep -n "*/" | tail -n -1 | cut -d ":" -f 1)
-            add_task_cron=$(cat $add_cron_list | head -$script_note_line | grep -oE '[0-9*]*[0-9,-/]{1,}\ [0-9*,-/]{1,}\ [0-9*,-?/LWC]{1,}\ [0-9*,-/]{1,}\ [0-9*,-?/LC#]{1,}' | head -n 1)
+            if [[ -n $script_note_line ]]; then
+                add_task_cron=$(cat $add_cron_list | head -$script_note_line | grep -oE '[0-9*]*[0-9,-/]{1,}\ [0-9*,-/]{1,}\ [0-9*,-?/LWC]{1,}\ [0-9*,-/]{1,}\ [0-9*,-?/LC#]{1,}' | head -n 1)
+            else
+                add_task_cron=''
+            fi
             if [[ -z $add_task_cron || -z $add_task_word ]]; then
                 notify_log "[ 任务添加失败 ] $add_task_name.js" $file_upcron_notify
             else
