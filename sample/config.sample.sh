89cffbcb05cabe3e909caf73921870531bbd3f71
alex <ffuqiangg@gmail.com>
2022-04-02 21:25:55 +0800

    新增排除脚本变量

--- sample/config.sample.sh
+++ sample/config.sample.sh
@@ -11,6 +11,9 @@ RmLogDaysAgo='7'
 ## 随机延迟最大值（秒），留空或为0则任务准时运行无延迟，如需设置最小延迟可自行添加 RandomMin 变量
 RandomMax='300'
 
+## 排除脚本(不添加任务cron)，填写完整脚本文件名(不带后缀)，多个脚本用空格隔开
+no_cron_list=""
+
 ## 独立js脚本文件
 OwnRawFile=(
   #https://raw.githubusercontent.com/Ariszy/Private-Script/master/JD/zy_xyzzh.js
